b706727baf9386c1c5f2b1cd82dbf78264a6e2ba
ClaireBvs <clairembeauvais@gmail.com>
2018-07-01 11:37:24 -0600

    implements comedian index page

--- spec/features/visitor_sees_comedians_homepage_spec.rb
+++ spec/features/visitor_sees_comedians_homepage_spec.rb
@@ -5,29 +5,30 @@
 #   * Name
 #   * Age
 
-RSpec.describe 'a visitor' do
-  context 'visiting /comedians' do
+RSpec.describe 'a user visit comedian index page' do
+  context 'visitor' do
     it 'should see a list of comedians including name and age' do
+      comedian_1 = Comedian.create(name: "Elie", age: 32)
+      comedian_2 = Comedian.create(name: "Maya", age: 21)
 
       visit '/comedians'
 
-      within('.comedians') do
-        expect(page).to have_content('Comedians')
-        # expect(page).to have_content('Name')
-        # expect(page).to have_content('Age')
+      expect(page).to have_content("Name: #{comedian_1.name}")
+      expect(page).to have_content("Age: #{comedian_1.age}")
+      expect(page).to have_content("Name: #{comedian_2.name}")
+      expect(page).to have_content("Age: #{comedian_2.age}")
       end
     end
 #     As a visitor,
 # When I visit `/comedians`
 # Then I also see a list of each comedian's specials.
 
-    it 'should see a list of comedians specials' do
-      visit '/comedians'
+    # it 'should see a list of comedians specials' do
+    #   visit '/comedians'
+    #
+    #   within('.comedians') do
+    #     expect(page).to have_content('Specials')
+    #   end
+    # end
 
-      within('.comedians') do
-        expect(page).to have_content('Specials')
-      end
-    end
-
-  end
 end
